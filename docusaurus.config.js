5b78e12ccfdde6e42356db8c98b4faa93a732c04
anwar <zhc17630380220@gmail.com>
2026-01-11 17:50:01 +0800

    just warning when Docusaurus found broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
